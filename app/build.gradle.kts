c40d8086ce23aa1e1fa2542d6c11aceac0d3fe69
Pia <algauer.henrique@gmail.com>
2024-09-28 13:23:41 -0300

    configurações para jitpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.github.henriquealgauer:criarPersonagemKT:1.0.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
